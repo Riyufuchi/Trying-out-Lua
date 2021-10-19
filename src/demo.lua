6b66edfd337ba300b60f6a09d4f3430d4189ddc2
Riyufuchi <56110060+Riyufuchi@users.noreply.github.com>
2021-10-19 22:49:12 +0200

    functions added

--- src/demo.lua
+++ src/demo.lua
@@ -1,10 +1,12 @@
 local code = require("code")
 local Color = require("Color")
+local util = require("linuxUtil")
 
 function main()
 	--textColor = Color:new(nil, 105, 255, 180)
 	textColor = Color:new(nil, 0, 255, 255)
 	code.createColoredHeader(textColor:getColor(), "Hello world, from " .. _VERSION)
+	util.updateColored(true)
 end
 
 main()
---
+++ src/linuxUtil.lua
@@ -0,0 +1,44 @@
+local linuxUtil =  {}
+local code = require("code")
+
+function linuxUtil.update(cleanAfter)
+	clean = cleanAfter or false
+	if(os.execute("sudo apt-get update")) then
+		if(os.execute("sudo apt-get upgrade")) then
+			print("Update succesful")
+		end
+	end
+	if(clean) then
+		if(os.execute("sudo apt-get autoremove")) then
+			if("sudo apt-get clean") then
+				print("Done!!")
+			end
+		end
+	end
+end
+
+function linuxUtil.updateColored(cleanAfter)
+	clean = cleanAfter or false
+	if(os.execute("sudo apt-get update")) then
+		code.coloredText(0, 255, 0, "Update done!")
+	else
+		code.coloredText(255, 0, 0, "Problem has occured!")
+	end
+	if(os.execute("sudo apt-get upgrade")) then
+		code.coloredText(0, 255, 0, "Upgrade done!")
+	else
+		code.coloredText(255, 0, 0, "Problem has occured!")
+	end
+	if(clean) then
+		code.coloredText(255, 255, 0, "Cleaning started")
+		if(os.execute("sudo apt-get autoremove")) then
+			code.coloredText(0, 255, 0, "Autoremove done!")
+		end
+		if("sudo apt-get clean") then
+				code.coloredText(0, 255, 0, "Clean done!")
+			end
+	end
+
+end
+
+return linuxUtil
