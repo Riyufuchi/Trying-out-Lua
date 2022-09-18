77ccfa3d5e377169c982b70cbbc6863001ddcd27
Riyufuchi <krevelixtv@gmail.com>
2022-09-18 17:50:59 +0200

    Update 0.1.2 Improved: * Changed apt-get to apt * Code readability

--- src/linuxUtil.lua
+++ src/linuxUtil.lua
@@ -1,60 +1,25 @@
 local linuxUtil = {}
 local code = require("code")
 
-function linuxUtil.update(cleanAfter)
+function linuxUtil.updateColored(cleanAfter)
 	clean = cleanAfter or false
-	if(os.execute("sudo apt-get update")) then
-		print("Update succesful")
-	else
-		print("Problem has occured!")
-	end
-	if(os.execute("sudo apt-get upgrade")) then
-		print("Update succesful")
-	else
-		print("Problem has occured!")
-	end
-	if(os.execute("sudo apt-get dist-upgrade")) then
-		print("Update succesful")
-	else
-		print("Problem has occured!")
-	end
+
+	OS_command("sudo apt update")
+	OS_command("sudo apt upgrade")
+	OS_command("sudo apt-get dist-upgrade")
 
 	if(clean) then
 		code.coloredText(255, 255, 0, "Cleaning started")
-		if(os.execute("sudo apt-get autoremove")) then
-			print("Autoremove done!")
-		end
-		if(os.execute("sudo apt-get clean")) then
-			print("Clean done!")
-		end
+		OS_command("sudo apt autoremove")
+		OS_command("sudo apt clean")
 	end
 end
 
-function linuxUtil.updateColored(cleanAfter)
-	clean = cleanAfter or false
-	if(os.execute("sudo apt-get update")) then
-		code.coloredText(0, 255, 0, "Update done!")
+function OS_command(command)
+	if(os.execute(command)) then
+		code.coloredText(0, 255, 0, command .. " - done!")
 	else
-		code.coloredText(255, 0, 0, "Problem has occured!")
-	end
-	if(os.execute("sudo apt-get upgrade")) then
-		code.coloredText(0, 255, 0, "Upgrade done!")
-	else
-		code.coloredText(255, 0, 0, "Problem has occured!")
-	end
-	if(os.execute("sudo apt-get dist-upgrade")) then
-		code.coloredText(0, 255, 0, "Dist-Upgrade done!")
-	else
-		code.coloredText(255, 0, 0, "Problem has occured!")
-	end
-	if(clean) then
-		code.coloredText(255, 255, 0, "Cleaning started")
-		if(os.execute("sudo apt-get autoremove")) then
-			code.coloredText(0, 255, 0, "Autoremove done!")
-		end
-		if(os.execute("sudo apt-get clean")) then
-			code.coloredText(0, 255, 0, "Clean done!")
-		end
+		code.coloredText(255, 0, 0, "Problem has occured during: " .. command)
 	end
 end
 
