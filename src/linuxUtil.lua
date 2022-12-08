2b14b0dc2cf0dac29c771b2eb1dc9de539d2181f
Riyufuchi <krevelixtv@gmail.com>
2022-12-08 14:14:15 +0100

    Update 0.1.6 Added: * Option for automated installation of multiple packages

--- src/linuxUtil.lua
+++ src/linuxUtil.lua
@@ -8,6 +8,10 @@ function linuxUtil.updateColored(cleanAfter)
 	OS_command("sudo apt update")
 	OS_command("sudo apt upgrade")
 	OS_command("sudo apt dist-upgrade")
+	if(console.askUser("Manual package install? [Y/n]")) then
+		io.write("Intput list of packages -> package1 package2...\n")
+		addMultiplePackages(io.read())
+	end
 	if(clean) then
 		code.coloredText(255, 255, 0, "Cleaning started!")
 		OS_command("sudo apt autoremove && sudo apt clean")
@@ -16,6 +20,13 @@ function linuxUtil.updateColored(cleanAfter)
 	code.coloredText(0, 255, 255, "Finished update sequence.")
 	if(console.askUser("Restart now? [Y/n]")) then
 		OS_command("sudo shutdown -r")
+	end
+end
+
+function addMultiplePackages(packageList)
+	--the "[^%s]+" pattern matches to every non-empty string in between space characters
+	for token in string.gmatch(packageList, "[^%s]+") do
+		OS_command("sudo apt install " .. token)
 	end
 end
 
