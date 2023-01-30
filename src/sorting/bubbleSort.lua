0e8babcd18b697f22b0dbe18d6eb7be917b0100e
Riyufuchi <krevelixtv@gmail.com>
2023-01-30 19:03:52 +0100

    Update 0.1.13 Added: * Bubble sort

---
+++ src/sorting/bubbleSort.lua
@@ -0,0 +1,30 @@
+local bubbleSort = {}
+local arr = {}
+
+function bubbleSort.sort(pArr)
+	if pArr == nil then
+		pArr = { -1 }
+	end
+	arr = pArr
+	len = #arr
+	if len <= 1 then
+		return arr
+	end
+	sorter(1, len)
+	return arr
+end
+
+function sorter(indexMin, indexMax)
+	local change = true
+	while change do
+		change = false
+		for i = indexMin, indexMax - 1 do
+			if arr[i] > arr[i + 1] then
+				arr[i], arr[i + 1] = arr[i + 1], arr[i]
+				change = true
+			end
+		end
+	end
+end
+
+return bubbleSort
